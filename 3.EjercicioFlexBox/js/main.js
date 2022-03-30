e8dbb2f53885c6a0e633b165c747cb0318bb6e3a
MJ <mfonsecap@ucenfotec.ac.cr>
2022-03-29 19:17:52 -0600

    v1

---
+++ 3.EjercicioFlexBox/js/main.js
@@ -0,0 +1,24 @@
+var countDownDate = new Date("Feb 21, 2023 21:00:00").getTime();
+var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+  
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+  
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    // Display the result in the elements
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+  }, 1000);
+
+  
